6116f165e10f6fff33912ed7f50b77e47892cdb4
jjerphan <julien.jerphanion@protonmail.com>
2017-02-19 11:35:43 +0100

    Functions for users extraction added

--- Tweets.py
+++ Tweets.py
@@ -17,7 +17,9 @@ def returnProfile(user,credentials):
     """Return the profile of the user whose the name 'user' was given"""
     global baseURL
     res = oauthRequest(baseURL+'users/show.json?screen_name='+user,credentials)
-    return json.load(StringIO(res))
+    user = json.load(StringIO(res))
+    user = cleanUser(user)
+    return user
 
 def returnTweet(user,credentials,nbTweet):
     """Return the last nbTweet he user 'user'"""
@@ -38,7 +40,7 @@ def cleanTweet(tweet):
     for key in fieldsToDelete:
         del tweet[key]
 
-    remainingStrFields = [k for k,v in tweet.items() if k in stringFields]
+    # remainingStrFields = [k for k,v in tweet.items() if k in stringFields]
     # for key in remainingStrFields:
     #     tweet[key] = str(tweet[key]).encode("utf-8")
 
@@ -46,6 +48,18 @@ def cleanTweet(tweet):
     del tweet["user"]
 
     return tweet
+
+
+def cleanUser(user):
+    """Clean a Tweet : delete the useless features and encode string in UTF8"""
+    global usefullFieldsUser
+    global stringFields
+
+    fieldsToDelete = [k for k,v in user.items() if k not in usefullFieldsUser]
+    for key in fieldsToDelete:
+        del user[key]
+
+    return user
 
 #####
 
@@ -67,38 +81,50 @@ credentials = [consumerKey,consumerSecret,key,secret]
 baseURL = "https://api.twitter.com/1.1/"
 
 
-# Fields (see : https://dev.twitter.com/overview/api/tweets):
+# Tweets' Fields (see : https://dev.twitter.com/overview/api/tweets):
 
-usefullFields = ["user","text","is_quote_status","in_reply_to_status_id","id_str",
-                "favorite_count","user_id","source","entities",
-                "in_reply_to_screen_name","in_reply_to_user_id","retweet_count",
+usefullFields = ["user","text","is_quote_status","in_reply_to_status_id","id",
+                "favorite_count","user_id","source","in_reply_to_user_id","retweet_count",
                 "lang","created_at"]
 
-# uselessFields = ["contributors","id","filter_level","geo","favorited",
+# uselessFields = ["contributors","id_str","filter_level","geo","favorited",
 #              "geo","in_reply_to_user_id_str","in_reply_to_status_id_str",
 #              "place","possibly_sensitive","quoted_status_id_str","quoted_status",
 #              "quoted_status_id","retweeted","retweeted_status","withheld_copyright"
 #              "withheld_in_countries","withheld_scope","favorited",
-#              "truncated","coordinates","extended_entities"]
+#              "truncated","coordinates","extended_entities","in_reply_to_screen_name","entities"]
 
-stringFields = ["created_at","filter_level,id_str","in_reply_to_screen_name","in_reply_to_status_id_str",
-              "in_reply_to_user_id_str","lang","quoted_status_id_str","source","withheld_scope"]
+stringFields = ["created_at","filter_level,id_str","in_reply_to_screen_name",
+                "in_reply_to_status_id_str","in_reply_to_user_id_str","lang",
+                "quoted_status_id_str","source","withheld_scope"]
 
-def main():
-    tweets = returnTweet("EmmanuelMacron",credentials,4)
-    for tweet in tweets:
-        print tweet["text"].encode("utf-8")
+# Users' Fields (see : https://dev.twitter.com/overview/api/users):
 
-    print "---------------"
+usefullFieldsUser = ["id","name","screen_name","created_at",
+                     "contributors_enabled","verified"]
+
+def testTweet():
+    tweets = returnTweet("EmmanuelMacron",credentials,1)
+
+    # for tweet in tweets:
+    #     print tweet["text"].encode("utf-8")
+    #
+    # print "---------------"
     remainingFields = [k for k,v in tweets[0].items()]
     for i in remainingFields:
-        print i, type(tweets[0][i])
-
+        print i,":", tweets[0][i]
+    print "---------------"
 # Two Null fields :
     # print tweets[0]["in_reply_to_status_id"]
     # print tweets[0]["in_reply_to_user_id"]
 
+def testProfile():
+    user = returnProfile("EmmanuelMacron",credentials)
 
+    remainingFields = [k for k,v in user.items()]
+    for i in remainingFields:
+        print i,":", user[i]
 
 if __name__ == '__main__':
-    main()
+    # testTweet()
+    testProfile()
