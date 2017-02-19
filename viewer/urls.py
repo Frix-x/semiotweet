2a67d1226cb9e5a22bebb86f3cc12cc99e703b50
jjerphan <julien.jerphanion@protonmail.com>
2017-02-19 13:04:46 +0100

    getUser : user info extraction ; to fix date format

--- viewer/urls.py
+++ viewer/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^displayAll/(?P<screen_name>.+)$',views.displayAll,name='displayAll'),
     url(r'^getTweets/(?P<screen_name>.+)/(?P<nbTweetToExtract>.+)$',views.getTweets,name='getTweets'),
-
+    url(r'^getUser/(?P<screen_name>.+)/$',views.getUser,name='getUser')
 ]
--- viewer/views.py
+++ viewer/views.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from django.http import HttpResponse,Http404
 from django.shortcuts import render,redirect
 from .models import Tweet,User
-import extraction
+from extraction import returnProfile,credentials
 
 ##
 import random
@@ -19,3 +19,26 @@ def displayAll(request,screen_name):
 def getTweets(request,screen_name,nbTweetToExtract):
 
     return HttpResponse(nbTweetToExtract+" tweets to extract from user :"+screen_name)
+
+def getUser(request,screen_name):
+    newUser = User()
+    userInfo = returnProfile(screen_name,credentials)
+    remainingFields = [k for k,v in userInfo.items()]
+    string = ""
+    for i in remainingFields:
+        string += i+":"+str(userInfo[i])+"<br/>"
+
+    newUser.id = userInfo['id']
+    newUser.name = userInfo['name']
+    newUser.screen_name = userInfo['screen_name']
+    newUser.created_at = userInfo['created_at']
+    newUser.contributors_enabled = userInfo['contributors_enabled']
+    newUser.verified = userInfo['verified']
+
+    #TODO : changing date format
+    newUser.save()
+    # try:
+    #     newUser.save()
+    # except BaseException:
+    #     string = "Utilisateur deja inscrit <br/>"+ string
+    return HttpResponse(string)
