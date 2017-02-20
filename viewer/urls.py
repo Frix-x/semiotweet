f0f5443a831346b512f55836e5405965faca24d4
jjerphan <julien.jerphanion@protonmail.com>
2017-02-20 16:26:21 +0100

    Redirecting homepage

--- viewer/urls.py
+++ viewer/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^displayAll/(?P<screen_name>.+)$',views.displayAll,name='displayAll'),
+    url(r'^$',views.home,name='home'),
+    url(r'^displayInfo/(?P<screen_name>.+)$',views.displayInfo,name='displayInfo'),
     url(r'^getTweets/(?P<screen_name>.+)/(?P<nbTweetToExtract>.+)$',views.getTweets,name='getTweets'),
     url(r'^getUser/(?P<screen_name>.+)/$',views.getUser,name='getUser')
 ]
