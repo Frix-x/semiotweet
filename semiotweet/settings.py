6176142c176607426d19ab24d44d8dfe0402586a
jjerphan <julien.jerphanion@protonmail.com>
2017-03-09 23:13:28 +0100

    Trying to fix Heroku PostGre DB

--- semiotweet/settings.py
+++ semiotweet/settings.py
@@ -147,8 +147,8 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
     }
 }
+DATABASES = { 'default' : dj_database_url.config()}
 
-# DATABASES = { 'default' : dj_database_url.config()}
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
