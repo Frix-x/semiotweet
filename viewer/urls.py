c97655eba8960283108ebaa29ede94b7fec27d7b
jjerphan <julien.jerphanion@protonmail.com>
2017-03-26 18:12:35 +0200

    Hack : hiding getData view for prod (to be improved!)

--- viewer/urls.py
+++ viewer/urls.py
@@ -4,8 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$',views.home,name='home'),
     url(r'^displayInfo/(?P<screen_name>.+)$',views.displayInfo,name='displayInfo'),
-    url(r'^getData/$',views.getData,name='getData'),
-    #url(r'^getWords/$',views.getWords,name='getWords'),
+#    url(r'^getData/$',views.getData,name='getData'),
     url(r'^displayNetwork/$',views.displayNetwork,name='displayNetwork'),
     url(r'(?P<typed>.+)$',views.handler404,name='handler404')
 ]
