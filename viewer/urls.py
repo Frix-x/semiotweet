6ba6cf4d2ffe4e69bac58e4c545a174fb39b7f2e
jjerphan <julien.jerphanion@protonmail.com>
2017-03-27 09:35:19 +0200

    For update

--- viewer/urls.py
+++ viewer/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$',views.home,name='home'),
     url(r'^displayInfo/(?P<screen_name>.+)$',views.displayInfo,name='displayInfo'),
-    url(r'^getData/$',views.getData,name='getData'),
+#    url(r'^getData/$',views.getData,name='getData'),
     url(r'^generalOverview/$',views.generalOverview,name='generalOverview'),
     url(r'^displayNetwork/$',views.displayNetwork,name='displayNetwork'),
     url(r'(?P<typed>.+)$',views.handler404,name='handler404')
