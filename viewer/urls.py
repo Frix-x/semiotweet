76acc14dfb31c0b7f392fdb54c94a80d0c3c59f0
Frix-x <boisselier.felix@gmail.com>
2017-03-30 16:34:15 +0200

    prevent diplayNetwork access for the moment

--- viewer/urls.py
+++ viewer/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
 #    url(r'^getData/$',views.getData,name='getData'),
     url(r'^generalOverview/?$',views.generalOverview,name='generalOverview'),
     url(r'^comparison/?$',views.comparison,name='comparison'),
-    url(r'^displayNetwork/?$',views.displayNetwork,name='displayNetwork'),
+#    url(r'^displayNetwork/?$',views.displayNetwork,name='displayNetwork'),
     url(r'(?P<typed>.+)$',views.handler404,name='handler404')
 ]
